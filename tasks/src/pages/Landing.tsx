cdd073e34107791a51c5ed5a19e26fc120bb201d
sasika bista <sasika.bista0809@gmail.com>
2026-02-25 11:43:34 +0545

    new project created task
     landing page started

---
+++ tasks/src/pages/Landing.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import hckCoreLogo from "../assets/hck_core_logo.svg"
+function Landing() {
+  return (
+    <div className="border-b border-[#68686833] p-[8px] flex justify-between items-center ">
+        <img src="" alt="" />
+    </div>
+  );
+}
+
+export default Landing;
