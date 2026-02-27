a53351f413ea38e572816f16e178818fa736a940
RiwajSt11 <122591238+RiwajSt11@users.noreply.github.com>
2026-02-27 17:02:45 +0545

    Update App.tsx

--- tasks/src/App.tsx
+++ tasks/src/App.tsx
@@ -1,11 +1,6 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-<<<<<<< Updated upstream
 import { Login } from "./pages/authentication/Login";
 import { Register } from "./pages/authentication/Register";
-=======
-import { Login } from "./pages/login/Login";
-import { Register } from "./pages/register/Register";
->>>>>>> Stashed changes
 import Landing from "./pages/Landing";
 import { Home } from "./pages/Home";
 function App() {
@@ -13,19 +8,10 @@ function App() {
     <>
       <BrowserRouter>
         <Routes>
-<<<<<<< Updated upstream
           <Route path="/" element={<Landing />} />
-=======
-          l<Route path="/" element={<Landing />} />
->>>>>>> Stashed changes
           <Route path="/login" element={<Login />} />
-<<<<<<< HEAD
           <Route path="/register" element={<Register />} />
           <Route path="/home" element={<Home />} />
-=======
-          l<Route path="/register" element={<Register />} />
-          l<Route path="/register" element={<Landing />} />
->>>>>>> 2243a14182ea3ecb7fbb11c5e1280368fbf614a0
         </Routes>
       </BrowserRouter>
     </>
